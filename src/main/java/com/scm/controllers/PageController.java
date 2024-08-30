2137cc320e5e24ebc93ccb608e89776f7906a551
Ayushrai83 <gurukul4ayush@gmail.com>
2024-08-30 18:12:32 +0530

    Project Setup

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,14 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home() {
+        System.out.println("Home page handler");
+        return "home";
+    }
+}
