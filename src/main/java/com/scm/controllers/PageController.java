ce4b0ca780593869137864248087545251c85cf7
Ayushrai83 <gurukul4ayush@gmail.com>
2024-08-31 13:23:20 +0530

    tailwindcss and flowbite setup

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,14 +1,21 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model model) {
         System.out.println("Home page handler");
+
+        //Sending data to view
+        model.addAttribute("name", "Substring Technologies");
+        model.addAttribute("linkdin", "https://in.linkedin.com/in/ayush-rai-gurukul-developer");
+        model.addAttribute("github", "https://github.com/Ayushrai83");
+
         return "home";
     }
 }
