08acc9443ad57c86f5e3ed7010ca2690393a9059
Ayushrai83 <gurukul4ayush@gmail.com>
2024-09-02 21:02:03 +0530

    Navbar and dark theme implementaion

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -2,6 +2,7 @@ package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,5 +39,28 @@ public class PageController {
         System.out.println("Services Page loading");
         return "services";
     }
+
+    // contact page
+
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+
+    //Login
+
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    //Register
+
+    @GetMapping("/register")
+    public String register() {
+        return new String("register");
+    }
+
+
     
 }
