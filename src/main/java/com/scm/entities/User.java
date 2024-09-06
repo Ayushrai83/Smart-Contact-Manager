71a6dc57ed6dc976178328b736b5cd5afcffb943
Ayushrai83 <gurukul4ayush@gmail.com>
2024-09-06 21:00:15 +0530

    Entities setup

--- src/main/java/com/scm/entities/User.java
+++ src/main/java/com/scm/entities/User.java
@@ -1,8 +1,14 @@
 package com.scm.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -39,4 +45,8 @@ public class User {
     //SELF.GOOGLE,FACEBOOK,TWITTER,LINKEDIN,GITHUB
     private Providers provider=Providers.SELF;
     private String providerUserId;
+
+    // add more fields if needed
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Contact> contacts = new ArrayList<>();
 }
